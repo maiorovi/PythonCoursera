92d6b5726d3605e744fc856f9d96814efe567d3a
tryharder <maiorog.prog@gmail.com>
2015-06-04 22:34:06 +0300

    implemented python scipts. playing with python variables.

--- src/Arithmetic.py
+++ src/Arithmetic.py
@@ -1,5 +1,5 @@
 # coding=UTF-8
-__author__ = 'Егор'
+__author__ = 'tryharder'
 
 print "HEllo", "Egor"
 
---
+++ src/Variables.py
@@ -0,0 +1,30 @@
+#illegal names of vairable
+# 12343name,
+
+# python convention is to use underscore symbol in variable names
+my_name = "Joe Warren"
+
+print my_name
+
+my_age = 50;
+
+print my_age
+
+magic_pill = 30;
+
+print my_age - magic_pill
+
+#Temperature example
+
+#convert from Fahrenheit to Celsius
+#c = 5 / 9 * (f-32)
+
+temp_Fahrenheit = 52
+temp_celsius = 5.0 / 9.0 * (temp_Fahrenheit - 32)
+print "Temperature in ceilsius: ", temp_celsius
+
+#convert from celsius to fahrenheit
+temp_celsius = 0
+
+temp_Fahrenheit = 9.0 / 5.0 * temp_celsius + 32
+print temp_Fahrenheit
