82c276bca42f861bb330fd1f1f27218c16bbce04
tryharder <maiorog.prog@gmail.com>
2015-06-04 22:23:56 +0300

    initial commit

---
+++ Arithmetic.py
@@ -0,0 +1,14 @@
+# coding=UTF-8
+__author__ = 'Егор'
+
+print "HEllo", "Egor"
+
+print 'Hello, world.'
+print "Hello, world"
+print 'She shouted "Hello" very loudly'
+print (7-2) / (3 ** 2)
+print (8 + (1 + (2 * 4) - 3))
+print 9 - (2 - (4 * 3))
+
+numb_er123 = 16
+__number__ =34;
