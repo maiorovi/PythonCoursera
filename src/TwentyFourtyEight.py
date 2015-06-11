bec26923fd59c80350b44b982db994b5f74a090a
tryharder <maiorog.prog@gmail.com>
2015-06-12 00:50:11 +0300

    * fixed unit  test and check style

--- src/TwentyFourtyEight.py
+++ src/TwentyFourtyEight.py
@@ -1,3 +1,4 @@
+""" this is fine program to play game 2048 """
 import random
 
 UP = 1
@@ -5,8 +6,6 @@ DOWN = 2
 LEFT = 3
 RIGHT = 4
 
-# Offsets for computing tile indices in each direction.
-# DO NOT MODIFY this dictionary.
 OFFSETS = {UP: (1, 0),
            DOWN: (-1, 0),
            LEFT: (0, 1),
@@ -18,28 +17,28 @@ def merge(line):
     """
     result_line = []
 
-    for i in range(0, len(line)):
-       if (line[i] == 0):
+    for iter in range(0, len(line)):
+       if (line[iter] == 0):
            continue
-       result_line.append(line[i])
-    for i in range(len(result_line), len(line)):
+       result_line.append(line[iter])
+    for iter in range(len(result_line), len(line)):
         result_line.append(0)
 
     final_result = []
-    i = 0
+    iter = 0
 
-    while i < len(result_line):
-        if (i + 1 == len(result_line)):
-            final_result.append(result_line[i])
-            i = i + 1
+    while iter < len(result_line):
+        if (iter + 1 == len(result_line)):
+            final_result.append(result_line[iter])
+            iter = iter + 1
             continue
-        if (result_line[i] == result_line[i+1]):
-            final_result.append(result_line[i] + result_line[i+1])
-            i = i + 2
-        elif (result_line[i] != result_line[i+1]):
-            final_result.append(result_line[i])
-            i = i + 1
-    for i in range(len(final_result), len(result_line)):
+        if (result_line[iter] == result_line[iter+1]):
+            final_result.append(result_line[iter] + result_line[iter+1])
+            iter = iter + 2
+        elif (result_line[iter] != result_line[iter+1]):
+            final_result.append(result_line[iter])
+            iter = iter + 1
+    for iter in range(len(final_result), len(result_line)):
         final_result.append(0)
 
     return final_result
@@ -50,24 +49,22 @@ class TwentyFortyEight:
     """
 
     def __init__(self, grid_height, grid_width):
-        self.grid_height = grid_height
-        self.grid_width = grid_width
-        self.up_initial_tile = [(0, x) for x in range(self.grid_width)]
-        self.down_initial_tile = [(self.grid_height - 1, x) for x in range(self.grid_width)]
-        self.left_initial_tile = [(x, 0) for x in range(self.get_grid_height())]
-        self.right_initial_tile = [(x, self.grid_width - 1) for x in range(self.get_grid_height())]
+        self._grid_height = grid_height
+        self._grid_width = grid_width
+        self._up_initial_tile = [(0, x) for x in range(self._grid_width)]
+        self._down_initial_tile = [(self._grid_height - 1, x) for x in range(self._grid_width)]
+        self._left_initial_tile = [(x, 0) for x in range(self.get_grid_height())]
+        self._right_initial_tile = [(x, self._grid_width - 1) for x in range(self.get_grid_height())]
         self.reset()
-        pass
 
     def reset(self):
         """
         Reset the game so the grid is empty except for two
         initial tiles.
         """
-        self.grid = [[0 for col in range(self.get_grid_width())] for row in range(self.get_grid_height())]
+        self._grid = [[0 for col in range(self.get_grid_width())] for row in range(self.get_grid_height())]
         self.new_tile()
         self.new_tile()
-        pass
 
     def __str__(self):
         """
@@ -77,7 +74,7 @@ class TwentyFortyEight:
 
         for row in range(self.get_grid_height()):
             for col in range(self.get_grid_width()):
-                result += str(self.grid[row][col]) + " "
+                result += str(self._grid[row][col]) + " "
             result += ("\n")
 
         return result
@@ -86,14 +83,14 @@ class TwentyFortyEight:
         """
         Get the height of the board.
         """
-        return self.grid_height
+        return self._grid_height
 
     def get_grid_width(self):
         """
         Get the width of the board.
         """
         # replace with your code
-        return self.grid_width
+        return self._grid_width
 
     def move(self, direction):
         """
@@ -106,41 +103,40 @@ class TwentyFortyEight:
 
         if (direction == UP):
             length = self.get_grid_height()
-            initial_tile = self.up_initial_tile
+            initial_tile = self._up_initial_tile
         if (direction == DOWN):
             length = self.get_grid_height()
-            initial_tile = self.down_initial_tile
+            initial_tile = self._down_initial_tile
         if (direction == LEFT):
-            length = self.get_grid_height()
-            initial_tile = self.left_initial_tile
+            length = self.get_grid_width()
+            initial_tile = self._left_initial_tile
         if (direction == RIGHT):
-            length = self.get_grid_height()
-            initial_tile = self.right_initial_tile
+            length = self.get_grid_width()
+            initial_tile = self._right_initial_tile
 
         for initial_cell in initial_tile:
             row = initial_cell[0]
             col = initial_cell[1]
             temp_list = []
 
-            for x in range(length):
-                temp_list.append(self.grid[row][col])
+            for counter in range(length):
+                temp_list.append(self._grid[row][col])
                 row = row + OFFSETS.get(direction)[0]
                 col = col + OFFSETS.get(direction)[1]
 
             row = initial_cell[0]
             col = initial_cell[1]
-            i = 0
-            for r in merge(temp_list):
-                if (temp_list[i] != r):
+            iterator = 0
+            for element in merge(temp_list):
+                if (temp_list[iterator] != element):
                     should_add_tile = True
-                i = i + 1
-                self.grid[row][col] = r
+                iterator = iterator + 1
+                self._grid[row][col] = element
                 row = row + OFFSETS.get(direction)[0]
                 col = col + OFFSETS.get(direction)[1]
 
         if (should_add_tile):
             self.new_tile()
-        pass
 
     def new_tile(self):
         """
@@ -150,7 +146,7 @@ class TwentyFortyEight:
         """
         col = random.randrange(self.get_grid_width())
         row = random.randrange(self.get_grid_height())
-        while (self.grid[row][col] != 0):
+        while (self._grid[row][col] != 0):
             col = random.randrange(self.get_grid_width())
             row = random.randrange(self.get_grid_height())
         value = random.randrange(0, 100)
@@ -160,24 +156,16 @@ class TwentyFortyEight:
         else:
             value = 2
 
-        self.grid[row][col] = value
-        pass
+        self._grid[row][col] = value
 
     def set_tile(self, row, col, value):
         """
         Set the tile at position row, col to have the given value.
         """
-        self.grid[row][col] = value
-        pass
+        self._grid[row][col] = value
 
     def get_tile(self, row, col):
         """
         Return the value of the tile at position row, col.
         """
-        return self.grid[row][col]
-
-object = TwentyFortyEight(4,4)
-
-print object
-object.move(1)
-print object
+        return self._grid[row][col]
